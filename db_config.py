ce0f8e27b0ea6fbc1dfbad3793d83e9f60523acc
CristianMarulandalo <cristian.marulandalo@amigo.edu.co>
2026-05-18 12:45:46 -0600

    Use environment variables for DB connection settings
    Refactor database connection to use environment variables.

--- db_config.py
+++ db_config.py
@@ -1,11 +1,12 @@
 import mysql.connector
+import os
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="123456",
-        database="tienda_flask",
-        port=3306,
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DB"),
+        port=int(os.getenv("MYSQL_PORT")),
         auth_plugin='mysql_native_password'
     )
